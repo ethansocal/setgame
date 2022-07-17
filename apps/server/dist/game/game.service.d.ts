552f81fcff39dc482bd3a0bb7d2c637b5d3a9861
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2022-07-16 22:41:15 -0700

    remove dist from git

--- apps/server/dist/game/game.service.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Socket } from "socket.io";
-export declare class GameService {
-    private connections;
-    connect(socket: Socket): void;
-    newPuzzle(socket: Socket): number[];
-    disconnect(socket: Socket): void;
-    checkSet(set: number[]): boolean;
-}
